276ff9b32745f94d0c3724a1091c31d86ff23720
Marcelo Mota <marcelomotadev@gmail.com>
2023-10-05 21:15:43 -0300

    Consertado grafo

--- appDistanciaCaminho.py
+++ appDistanciaCaminho.py
@@ -21,24 +21,24 @@ def bfs_com_caminho(lista_vizinhos, vertice_inicial, vertice_destino):
     return -1
 
 # Vértices de origem e destino
-vertice_origem = 'A'
-vertice_destino = 'T'
+vertice_origem = 'N'
+vertice_destino = 'O'
 
 lista_vizinhos = { 
   'A': ['B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'], 
   'B': ['A', 'G', 'H'],
-  'C': ['A', 'D', 'I'], 
-  'D': ['A', 'C', 'I', 'N', 'L'], 
+  'C': ['A', 'I', 'N'], 
+  'D': ['A', 'I', 'N', 'L'], 
   'E': ['A', 'F'], 
   'F': ['A', 'E', 'J'], 
   'G': ['A', 'B', 'I', 'J', 'K'], 
   'H': ['A', 'B', 'I'], 
-  'I': ['A', 'C', 'D', 'H', 'N', 'G', 'R', 'Q'], 
+  'I': ['A', 'C', 'D', 'H', 'G', 'R', 'Q'], 
   'J': ['G', 'F'], 
   'K': ['G', 'P', 'Q', 'O'], 
   'L': ['D', 'M'], 
   'M': ['L', 'N'], 
-  'N': ['I', 'D', 'M', 'S'], 
+  'N': ['D', 'M', 'S'], 
   'O': ['K'], 
   'P': ['K', 'U'], 
   'Q': ['K', 'U', 'I'], 
